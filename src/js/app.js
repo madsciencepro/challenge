cc7d00e382bc7e106894b5bff3577462380f324d
Tony Wilson <tony.wilson@uncp.edu>
2021-05-20 16:24:43 -0400

    almost done

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,20 @@
-console.log('Testing');
+// Fade Cards in on page load.
+
+var opacity = 0;
+var intervalID = 0;
+window.onload = fadeIn;
+
+function fadeIn() {
+  setInterval(show, 100);
+}
+
+function show() {
+  var body = document.getElementById('cards');
+  opacity = Number(window.getComputedStyle(cards).getPropertyValue('opacity'));
+  if (opacity < 1) {
+    opacity = opacity + 0.1;
+    body.style.opacity = opacity;
+  } else {
+    clearInterval(intervalID);
+  }
+}
